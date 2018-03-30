7805dba627f260b118d5afe6625a3e0169c6c43b
Ojesav <ojesav990@gmail.com>
2018-03-30 20:15:42 +0530

    Date Commit

---
+++ contract/include/date_functions.js
@@ -0,0 +1,4 @@
+var datediff=function(dt1,dt2)
+{
+	return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+}
